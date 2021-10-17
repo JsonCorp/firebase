549111ec42f12990f5883609bfc325df42c88650
Json <sonxxga@gmail.com>
2021-10-17 21:01:07 +0900

    updated crashlytics

--- app/src/main/java/com/json/client/firebasetest/MainActivity.java
+++ app/src/main/java/com/json/client/firebasetest/MainActivity.java
@@ -3,20 +3,31 @@ package com.json.client.firebasetest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        //Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         // Crashlytics 테스트를 위해 강제로 crash 발생
 //        Crashlytics.getInstance().crash(); // Force a crash
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Test Crash");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 }
